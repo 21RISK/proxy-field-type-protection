eb1ee3b1c8f88bf14adf1137a87d42f34b50b8a9
Alex Bjørlig <alexbjorlig@icloud.com>
2024-02-26 22:45:48 +0100

    sync

--- src/routes/hmm/+page.svelte
+++ src/routes/hmm/+page.svelte
@@ -2,17 +2,34 @@
   import { defaults, formFieldProxy, superForm } from 'sveltekit-superforms';
 	import { userSchema } from './schema.js';
 	import { zod } from 'sveltekit-superforms/adapters';
+	import { onMount } from 'svelte';
+	import { get } from 'svelte/store';
+	  
 
-	
-    const formData = defaults(
-      /**
-       * Undefined should not be allowed here.
-      */
-        { name: 'First name', lastName: undefined },
-        zod(userSchema)
-    )
+    const formData = defaults({ name: 'First name'}, zod(userSchema))
 
-   
+    export const spForm = superForm(formData, {
+      validators: zod(userSchema),
+      validationMethod: 'auto',
+      dataType: 'form',
+      onSubmit: (e) => {
+        console.log('onSubmit',  {data: [...e.formData]}) // <== Will differ from in-memory data
+      },
+    });
+
+    const {enhance, form}  = spForm;
+
+    const fieldProxy = formFieldProxy(spForm, 'name');
+
+    onMount(() => {
+      fieldProxy.value.set(undefined as any);
+      console.log('In-memory data', get(spForm.form)); // <== Compare to onSubmit
+      spForm.submit();
+    });
   </script>
-
+  
+<form use:enhance method="POST">
+    <input name="name" bind:value={$form.name} />
+    <button type="submit">Save</button>
+</form>
   
--- src/routes/hmm/schema.ts
+++ src/routes/hmm/schema.ts
@@ -2,8 +2,6 @@ import { z } from "zod";
 
 export const userSchema = z.object({
     name: z.string(),
-    lastName: z.string().trim().nullable()
-    
 })
 
 
